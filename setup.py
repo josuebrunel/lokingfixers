ddd021438676aaa9755000e0303e743b8a3f145d
josuebrunel <josuebrunel@gmail.com>
2015-01-09 06:57:57 +0100

    Cleaning the cat house

--- setup.py
+++ setup.py
@@ -11,16 +11,8 @@ setup(
     author='josue kouka',
     author_email='josuebrunel@gmail.com',
     license='MIT',
-    # packages=[
-    #     'lokingfixers', 
-    #     'lokingfixers.fixers', 
-    #     'lokingfixers.fixers.linux',
-    #     'lokingfixers.fixers.osx',
-    #     'lokingfixers.tools',
-    #     'lokingfixers.models',
-    # ],
     packages=find_packages(),
     zip_safe=False,
-    install_requires= required,
+    install_requires=required,
     scripts=['lokingfixers.py'],
 )
