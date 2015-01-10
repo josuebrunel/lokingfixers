add2dd3f6d7626faf2b3d2086150c22e034b90bb
josuebrunel <josuebrunel@gmail.com>
2015-01-10 09:16:08 +0100

    updates #1 , closes #1 : settings are making sense now

--- lokingfixers/settings.py
+++ lokingfixers/settings.py
@@ -6,8 +6,10 @@ hooking_time = datetime.now().strftime('%d-%m-%Y-%H-%M')
 ##ATTRIBUTS
 ATTRIBUT_KEY= 'com.apple.FinderInfo'
 
+SETTINGS_DIR = '.lokingfixers/logs' 
+
 ##LOG SETTINGS
-LOG_FOLDER = 'lokingfixers/logs'
+LOG_FOLDER = SETTINGS_DIR
 LOG_FILE_SIZE = 1 * 1024 * 1024
 LOG_FILENAME = os.path.join(LOG_FOLDER, datetime.now().strftime('log-%d-%m-%Y-%H-%M.log'))
 LOG_REPORT = os.path.join(LOG_FOLDER, datetime.now().strftime('report-%d-%m-%Y-%H-%M.log'))
@@ -15,10 +17,17 @@ LOG_ERROR = os.path.join(LOG_FOLDER, datetime.now().strftime('error-%d-%m-%Y-%H-
 
 ##HOME CONFIG FILES SETTINGS
 
-HOME_CONFIG = {
-    'src' : os.environ['HOME'],
-    'dest' : os.path.join(os.environ['HOME'],'workspace/home/'),
-    'files' : [
+SRC = os.path.expanduser('~')
+
+# Where to put the archive
+DEST_FOLDER = ''
+DEST = os.path.join(SRC,DEST_FOLDER)
+
+# Name of the archive
+ARCHIVE = 'HomeDirectoryConfig.tar.gz'
+
+# files to backup
+FILES_LIST = [
         '.profile',
         '.pythonenv.py',
         '.emacs',
@@ -26,7 +35,12 @@ HOME_CONFIG = {
         '.inputrc',
         '.gitconfig',
         '.pdbrc',
-    ],
+    ]
+
+HOME_CONFIG = {
+    'src' : SRC,
+    'dest' : DEST,
+    'files' : FILES_LIST,
     'archive' : 'HomeDirectoryConfig.tar.gz'
 }
 
